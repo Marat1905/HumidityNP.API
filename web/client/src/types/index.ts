3a1cce6e8f52f96484f1729f9ea0ad5bce513813
Marat <milya010790@mail.ru>
2026-07-21 16:19:59 +0500

    Забыли добавить PagedResult

--- web/client/src/types/index.ts
+++ web/client/src/types/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello World!")
+
+/**
+ * Обёртка для пагинированного ответа от API.
+ * @template T Тип элементов на странице.
+ */
+export interface PagedResult<T> {
+    /** Элементы текущей страницы */
+    items: T[];
+    /** Общее количество записей */
+    totalCount: number;
+    /** Номер текущей страницы */
+    pageNumber: number;
+    /** Размер страницы */
+    pageSize: number;
+    /** Общее количество страниц */
+    totalPages: number;
+}
