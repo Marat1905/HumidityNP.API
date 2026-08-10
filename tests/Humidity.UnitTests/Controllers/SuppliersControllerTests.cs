ce0eff2beaf50a42ada997ae29a59cea7e58f6c9
Marat <milya010790@mail.ru>
2026-08-10 09:37:50 +0500

    Тесты

--- tests/Humidity.UnitTests/Controllers/SuppliersControllerTests.cs
+++ tests/Humidity.UnitTests/Controllers/SuppliersControllerTests.cs
@@ -1,18 +1,174 @@
-﻿using Humidity.API.Controllers;
+﻿using FluentAssertions;
+using Humidity.API.Controllers;
 using Humidity.Application.Interfaces;
+using Humidity.Domain.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
+using Xunit;
 
 namespace Humidity.UnitTests.Controllers;
 
 public class SuppliersControllerTests
 {
-    private readonly Mock<ISupplierService> _serviceMock;
+    private readonly Mock<ISupplierService> _supplierServiceMock;
     private readonly SuppliersController _controller;
 
     public SuppliersControllerTests()
     {
-        _serviceMock = new Mock<ISupplierService>();
-        _controller = new SuppliersController(_serviceMock.Object);
+        _supplierServiceMock = new Mock<ISupplierService>();
+        _controller = new SuppliersController(_supplierServiceMock.Object);
+
+        // Инициализация HttpContext для предотвращения NullReferenceException 
+        // при обращении к HttpContext.RequestAborted в методах контроллера
+        _controller.ControllerContext = new ControllerContext
+        {
+            HttpContext = new DefaultHttpContext()
+        };
     }
 
+    [Fact]
+    public async Task GetSuppliers_WithValidParameters_ReturnsOkWithPagedResult()
+    {
+        // Arrange
+        var from = DateTimeOffset.UtcNow.AddDays(-7);
+        var to = DateTimeOffset.UtcNow;
+        var suppliers = new List<SupplierDto>
+        {
+            new SupplierDto
+            {
+                Inn = "7707083893",
+                Counterparty = "Test Supplier LLC",
+                VehiclesCount = 5,
+                TotalMeasurements = 10,
+                AverageHumidity = 15.5,
+                MinHumidity = 10.0,
+                MaxHumidity = 20.0
+            }
+        };
+        var pagedResult = new PagedResult<SupplierDto>
+        {
+            Items = suppliers,
+            TotalCount = 1,
+            PageNumber = 1,
+            PageSize = 20,
+            TotalPages = 1
+        };
+
+        _supplierServiceMock
+            .Setup(s => s.GetSuppliersAsync(from, to, 1, 20, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(pagedResult);
+
+        // Act
+        var result = await _controller.GetSuppliers(from, to, 1, 20);
+
+        // Assert
+        var okResult = result.Should().BeOfType<OkObjectResult>().Subject;
+        okResult.StatusCode.Should().Be(200);
+        okResult.Value.Should().BeEquivalentTo(pagedResult);
+    }
+
+    [Fact]
+    public async Task GetSupplierDetails_WhenVehiclesExist_ReturnsOk()
+    {
+        // Arrange
+        var inn = "7707083893";
+        var from = DateTimeOffset.UtcNow.AddDays(-7);
+        var to = DateTimeOffset.UtcNow;
+        var details = new SupplierDetailsDto
+        {
+            Inn = inn,
+            Counterparty = "Test Supplier LLC",
+            Vehicles = new List<SupplierVehicleSummaryDto>
+            {
+                new SupplierVehicleSummaryDto
+                {
+                    VehicleId = Guid.NewGuid(),
+                    Number = "V001",
+                    VehiclePlate = "A123BC",
+                    EntryDate = DateTimeOffset.UtcNow.AddDays(-5),
+                    ExitDate = null,
+                    MeasurementsCount = 3,
+                    AverageHumidity = 15.5,
+                    MinHumidity = 12.0,
+                    MaxHumidity = 18.0,
+                    AutoCount = 2,
+                    ManualCount = 1,
+                    LastMeasurementTimestamp = DateTimeOffset.UtcNow.AddHours(-2)
+                }
+            },
+            OverallStatistics = new MeasurementStatisticsDto()
+        };
+
+        _supplierServiceMock
+            .Setup(s => s.GetSupplierDetailsAsync(inn, from, to, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(details);
+
+        // Act
+        var result = await _controller.GetSupplierDetails(inn, from, to);
+
+        // Assert
+        var okResult = result.Should().BeOfType<OkObjectResult>().Subject;
+        okResult.StatusCode.Should().Be(200);
+        okResult.Value.Should().BeEquivalentTo(details);
+    }
+
+    [Fact]
+    public async Task GetSupplierDetails_WhenVehiclesEmpty_ReturnsNotFound()
+    {
+        // Arrange
+        var inn = "7707083893";
+        var from = DateTimeOffset.UtcNow.AddDays(-7);
+        var to = DateTimeOffset.UtcNow;
+        var details = new SupplierDetailsDto
+        {
+            Inn = inn,
+            Counterparty = "Test Supplier LLC",
+            Vehicles = new List<SupplierVehicleSummaryDto>(), // Пустой список машин
+            OverallStatistics = new MeasurementStatisticsDto()
+        };
+
+        _supplierServiceMock
+            .Setup(s => s.GetSupplierDetailsAsync(inn, from, to, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(details);
+
+        // Act
+        var result = await _controller.GetSupplierDetails(inn, from, to);
+
+        // Assert
+        result.Should().BeOfType<NotFoundObjectResult>();
+    }
+
+    [Fact]
+    public async Task GetTopSuppliers_WithValidParameters_ReturnsOk()
+    {
+        // Arrange
+        var from = DateTimeOffset.UtcNow.AddDays(-7);
+        var to = DateTimeOffset.UtcNow;
+        var topSuppliers = new List<SupplierDto>
+        {
+            new SupplierDto
+            {
+                Inn = "7707083893",
+                Counterparty = "Top Supplier LLC",
+                VehiclesCount = 10,
+                TotalMeasurements = 50,
+                AverageHumidity = 12.5,
+                MinHumidity = 8.0,
+                MaxHumidity = 17.0
+            }
+        };
+
+        _supplierServiceMock
+            .Setup(s => s.GetTopSuppliersAsync(10, true, from, to, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(topSuppliers);
+
+        // Act
+        var result = await _controller.GetTopSuppliers(from, to, 10, "asc");
+
+        // Assert
+        var okResult = result.Should().BeOfType<OkObjectResult>().Subject;
+        okResult.StatusCode.Should().Be(200);
+        okResult.Value.Should().BeEquivalentTo(topSuppliers);
+    }
 }
--- tests/Humidity.UnitTests/Controllers/VehiclesControllerTests.cs
+++ tests/Humidity.UnitTests/Controllers/VehiclesControllerTests.cs
@@ -1,17 +1,133 @@
-﻿using Humidity.API.Controllers;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Humidity.API.Controllers;
+using Humidity.Application.DTOs;
 using Humidity.Application.Interfaces;
+using Humidity.Domain.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
+using Xunit;
 
 namespace Humidity.UnitTests.Controllers;
+
 public class VehiclesControllerTests
 {
-    private readonly Mock<IVehicleService> _serviceMock;
+    private readonly Mock<IVehicleService> _vehicleServiceMock;
     private readonly VehiclesController _controller;
 
     public VehiclesControllerTests()
     {
-        _serviceMock = new Mock<IVehicleService>();
-        _controller = new VehiclesController(_serviceMock.Object);
+        _vehicleServiceMock = new Mock<IVehicleService>();
+        _controller = new VehiclesController(_vehicleServiceMock.Object);
+
+        // Инициализация HttpContext для предотвращения NullReferenceException
+        _controller.ControllerContext = new ControllerContext
+        {
+            HttpContext = new DefaultHttpContext()
+        };
+    }
+
+    [Fact]
+    public async Task GetAll_WithValidParameters_ReturnsOkWithPagedResult()
+    {
+        // Arrange
+        var vehicles = new List<VehicleDto> { new VehicleDto { Id = Guid.NewGuid(), VehiclePlate = "A123BC" } };
+        var pagedResult = new PagedResult<VehicleDto>
+        {
+            Items = vehicles,
+            TotalCount = 1,
+            PageNumber = 1,
+            PageSize = 20,
+            TotalPages = 1
+        };
+
+        _vehicleServiceMock
+            .Setup(s => s.GetFilteredPagedAsync(1, 20, null, true, null, null, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(pagedResult);
+
+        // Act
+        var result = await _controller.GetAll(1, 20, null, "active", null, null);
+
+        // Assert
+        var okResult = result.Should().BeOfType<OkObjectResult>().Subject;
+        okResult.StatusCode.Should().Be(200);
+        okResult.Value.Should().BeEquivalentTo(pagedResult);
+    }
+
+    [Fact]
+    public async Task GetById_WhenVehicleExists_ReturnsOk()
+    {
+        // Arrange
+        var vehicleId = Guid.NewGuid();
+        var vehicle = new VehicleDto { Id = vehicleId, VehiclePlate = "A123BC" };
+
+        _vehicleServiceMock
+            .Setup(s => s.GetByIdAsync(vehicleId, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(vehicle);
+
+        // Act
+        var result = await _controller.GetById(vehicleId);
+
+        // Assert
+        var okResult = result.Should().BeOfType<OkObjectResult>().Subject;
+        okResult.Value.Should().BeEquivalentTo(vehicle);
+    }
+
+    [Fact]
+    public async Task GetById_WhenVehicleNotFound_ReturnsNotFound()
+    {
+        // Arrange
+        var vehicleId = Guid.NewGuid();
+
+        _vehicleServiceMock
+            .Setup(s => s.GetByIdAsync(vehicleId, It.IsAny<CancellationToken>()))
+            .ReturnsAsync((VehicleDto?)null);
+
+        // Act
+        var result = await _controller.GetById(vehicleId);
+
+        // Assert
+        result.Should().BeOfType<NotFoundObjectResult>();
+    }
+
+    [Fact]
+    public async Task Create_WithValidRequest_ReturnsCreatedAtAction()
+    {
+        // Arrange
+        var request = new CreateVehicleRequest { VehiclePlate = "A123BC", Counterparty = "Test Supplier" };
+        var createdVehicle = new VehicleDto { Id = Guid.NewGuid(), VehiclePlate = "A123BC" };
+
+        _vehicleServiceMock
+            .Setup(s => s.CreateAsync(request, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(createdVehicle);
+
+        // Act
+        var result = await _controller.Create(request);
+
+        // Assert
+        var createdResult = result.Should().BeOfType<CreatedAtActionResult>().Subject;
+        createdResult.ActionName.Should().Be(nameof(VehiclesController.GetById));
+        createdResult.Value.Should().BeEquivalentTo(createdVehicle);
+    }
+
+    [Fact]
+    public async Task Delete_WithValidId_ReturnsNoContent()
+    {
+        // Arrange
+        var vehicleId = Guid.NewGuid();
+
+        _vehicleServiceMock
+            .Setup(s => s.DeleteAsync(vehicleId, It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask);
+
+        // Act
+        var result = await _controller.Delete(vehicleId);
+
+        // Assert
+        result.Should().BeOfType<NoContentResult>();
     }
 }
-
