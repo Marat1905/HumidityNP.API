6636ee11f12f788cc884df1e863dcc1a7b07d9ce
Marat <milya010790@mail.ru>
2026-09-07 20:45:13 +0500

    подправили

--- web/client/src/pages/humidity/VehicleDetailsPage.tsx
+++ web/client/src/pages/humidity/VehicleDetailsPage.tsx
@@ -82,7 +82,7 @@ export default function VehicleDetailsPage() {
     };
 
     // Формируем путь для ссылки «Главная» с сохранением параметров пагинации
-    const mainPagePath = searchParams.toString() ? `/?${searchParams.toString()}` : '/humidity';
+    const mainPagePath = searchParams.toString() ? `/humidity/?${searchParams.toString()}` : '/humidity';
 
     return (
         <div>
