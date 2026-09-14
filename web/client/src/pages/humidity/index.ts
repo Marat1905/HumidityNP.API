dfd13ecc13c0dde09670950db9b30f101141711e
Marat <milya010790@mail.ru>
2026-09-14 16:51:33 +0500

    Удалили временно

--- web/client/src/pages/humidity/index.ts
+++
@@ -1,9 +0,0 @@
-
-export { default as HumidityPage } from './HumidityPage';
-export { default as MeasurementsPage } from './MeasurementsPage';
-export { default as ReportPeriodPage } from './ReportPeriodPage';
-export { default as ShiftReportsPage } from './ShiftReportsPage';
-export { default as SuppliersPage } from './SuppliersPage';
-export { default as TopSuppliersPage } from './TopSuppliersPage';
-export { default as VehicleDetailsPage } from './VehicleDetailsPage';
-export { default as VehiclesPage } from './VehiclesPage';
