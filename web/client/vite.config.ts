54edb8dcef42e7359bcea39c66e1bb5dcce62f55
Marat <milya010790@mail.ru>
2026-09-16 13:37:25 +0500

    Добавили в прокси

--- web/client/vite.config.ts
+++ web/client/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
     server: {
@@ -10,7 +9,12 @@ export default defineConfig({
             '/humidity/api': {
                 target: 'http://localhost:8090',
                 changeOrigin: true,
-            }
+            },
+            '/hubs': {
+                target: 'http://localhost:8090',
+                changeOrigin: true,
+                ws: true,
+            },
         }
     }
 })
